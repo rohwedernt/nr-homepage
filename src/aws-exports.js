66ae9d255a34c509f382fcd1ceb24977c4b34f5d
rohwedernt <rohwedernt@gmail.com>
2021-06-29 12:01:15 -0600

    Update appsync api key

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -5,13 +5,12 @@ const awsmobile = {
     "aws_appsync_graphqlEndpoint": "https://rwlimck4lnf67gl5fckyqvheem.appsync-api.us-east-2.amazonaws.com/graphql",
     "aws_appsync_region": "us-east-2",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-rdkuf7urkrbpfknnws7s3qiisu",
+    "aws_appsync_apiKey": "da2-2xkwggxxpvg2tmrcqnrtwq4jfa",
     "aws_cognito_identity_pool_id": "us-east-2:86aaccb7-56b1-4506-a9f8-19805dd38690",
     "aws_cognito_region": "us-east-2",
     "aws_user_pools_id": "us-east-2_rDSoxcOFd",
     "aws_user_pools_web_client_id": "2mr8ig7j7d5kp1arfsm5gugkff",
     "oauth": {}
 };
-
 
 export default awsmobile;
