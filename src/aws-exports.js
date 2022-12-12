6ec2e7fd942ff5f7d31e2d6633b95e70be563bcf
Nate Rohweder <rohwedernt@gmail.com>
2022-12-11 20:41:28 -0700

    Update aws-exports.js

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -5,7 +5,7 @@ const awsmobile = {
     "aws_appsync_graphqlEndpoint": "https://rwlimck4lnf67gl5fckyqvheem.appsync-api.us-east-2.amazonaws.com/graphql",
     "aws_appsync_region": "us-east-2",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-r6vc4ncnfvh3vnrsfcbydjcyza",
+    "aws_appsync_apiKey": "da2-oze2uj5qwvflpgf2g5lcg7jnga",
     "aws_cognito_identity_pool_id": "us-east-2:86aaccb7-56b1-4506-a9f8-19805dd38690",
     "aws_cognito_region": "us-east-2",
     "aws_user_pools_id": "us-east-2_rDSoxcOFd",
